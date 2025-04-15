ad451d8cefc34182be2a1f51b322beec96b90db0
Akash Kumar <mrakashhero@gmail.com>
2025-04-15 13:45:08 +0530

    Update the model to v5

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
 # Set the version manually
-model_version = "v4.0"
+model_version = "v5.0"
 model_filename = f"model_{model_version}.pkl"
 
 # Train the model
